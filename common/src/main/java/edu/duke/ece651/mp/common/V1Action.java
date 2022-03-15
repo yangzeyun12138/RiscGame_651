6898513c45101d5cd6844e7df0092770ff47f0a4
Ching-Hang Hsu <ch450@duke.edu>
2022-03-15 16:28:50 -0400

    add the check function in V1Action.java

--- common/src/main/java/edu/duke/ece651/mp/common/V1Action.java
+++ common/src/main/java/edu/duke/ece651/mp/common/V1Action.java
@@ -3,6 +3,14 @@ package edu.duke.ece651.mp.common;
 import java.util.*;
 import java.util.Random;
 public class V1Action implements AbstractActionFactory {
+
+  @Override
+  public String checkForMove(Player player, String src, String dest, int numUnit){
+    MoveChecker UnitCheck = new UnitRuleChecker(null);
+    MoveChecker PathCheck = new PathRuleChecker(UnitCheck);
+    String result = PathCheck.checkMovement(player, src, dest, numUnit);
+    return result;
+  }  
    /**
    *Move function is to help the player to move the units from source territory to the destination territory.
    *@param player build the attack!
@@ -12,9 +20,7 @@ public class V1Action implements AbstractActionFactory {
    */
   @Override
   public void Move(Player player, String src, String dest, int numUnit){
-    MoveChecker UnitCheck = new UnitRuleChecker(null);
-    MoveChecker PathCheck = new PathRuleChecker(UnitCheck);
-    String result = PathCheck.checkMovement(player, src, dest, numUnit);
+    String result = checkForMove(player, src, dest, numUnit);
     if(result == null){
       // find src territory and dest territory
       for(Territory curr_t: player.player_terri_set){
@@ -31,9 +37,37 @@ public class V1Action implements AbstractActionFactory {
     } 
   }
 
-  // TODO: commit to server
+  /**
+   * checkForAttack() would check whether the attack if legal or not.
+   * @param attacker is the Player who attacks others
+   * @param defender is the Player who is attacked by others
+   * @param src is the name of the source territory
+   * @param dest is the name of the destination territory
+   * @param numUnit is the number of Unit that join the attack.
+   * @return returns null if there is no error. returns string argument when there is an error.
+   */
+  public String checkForAttack(Player attacker, Player defender, String src, String dest, int numUnit){
+    AttackChecker AdjacencyCheck = new AdjacencyRuleChecker(null);
+    AttackChecker FactionCheck = new FactionRuleChecker(AdjacencyCheck);
+    String result = FactionCheck.checkAttack(attacker, src, dest, numUnit);
+    return result;
+  }
+
+  /**
+   * Attack() would implement the command of attack.
+   * @param attacker is the Player who attacks others
+   * @param defender is the Player who is attacked by others
+   * @param src is the name of the source territory
+   * @param dest is the name of the destination territory
+   * @param numUnit is the number of Unit that join the attack.
+   */
   @Override
   public void Attack (Player attacker, Player defender, String src, String dest, int numUnit){
+    String checkResult = checkForAttack(attacker,defender,src, dest,numUnit);
+    if (checkResult != null){
+      throw new IllegalArgumentException(checkResult);
+    }
+    
     Territory attackerTerri = findTerritory(attacker, src);
     Territory defenderTerri = findTerritory(defender, dest);
     // Attacker lose units that is attacking
@@ -58,9 +92,15 @@ public class V1Action implements AbstractActionFactory {
     
   }
 
+  /**
+   * rollDice() would help the user to determine the winner of a round of attack
+   * @note You can change the seed1 and seed2.
+   * @note You can get actual random seed by using long time_seed.
+*/
   public boolean rollDice(){
     int min = 1;
     int max = 20;
+    long time_seed = System.currentTimeMillis();
     int seed1 = 100;
     int seed2 = seed1+1;
     Random rand1 = new Random(seed1);
@@ -78,6 +118,12 @@ public class V1Action implements AbstractActionFactory {
     }
   }
 
+  /**
+   * findTerritory() can help you find the territory from a player.
+   * @param player is the owner of the territory.
+   * @param name is the name of the territory.
+   * @return returns the territory.
+   */
   public Territory findTerritory(Player player, String name){
     for(Territory t : player.player_terri_set){
       if(t.getName().equals(name)){
