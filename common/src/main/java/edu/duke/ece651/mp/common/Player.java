70fcdbfa7c5e0102b2605e86375fd0e36cda39fd
Ching-Hang Hsu <ch450@duke.edu>
2022-04-16 18:46:34 -0400

    finish updateViewMap() and viewMapInitializer() in Player.java. And then pass the testcases in PlayerTest.java

--- common/src/main/java/edu/duke/ece651/mp/common/Player.java
+++ common/src/main/java/edu/duke/ece651/mp/common/Player.java
@@ -21,44 +21,7 @@ public class Player implements java.io.Serializable{
         this.old_view_map = viewMapInitializer(player_terri_set);
         this.new_view_map = viewMapInitializer(player_terri_set);
     }
-
-  public HashMap<String, ArrayList<Territory>> viewMapInitializer(HashSet<Territory> player_terri_set){
-    ArrayList<Territory> current_viewed_territory = new ArrayList<Territory>();
-    ArrayList<Territory> current_grey_territory = new ArrayList<Territory>();
-    HashSet<String> current_viewed_territory_name = new HashSet<String>();
-
-    // Adding the Territories that the player owns
-    for(Territory t : player_terri_set){
-      current_viewed_territory.add(t);
-      current_viewed_territory_name.add(t.getName());
-    }
-
-    // Adding the Neighbors of those Territories
-    ArrayList<Territory> current_viewed_territory_copy = new ArrayList<Territory>();
-    // 1. Deep copy
-    for(Territory t : current_viewed_territory){
-      Territory copy = t.deep_copy();
-      current_viewed_territory_copy.add(copy);
-    }
-    
-    // 2. Traverse Neighbors now
-    for(Territory terri : current_viewed_territory_copy){
-      for(Territory neigh : terri.getNeigh()){
-        // current_viewed_territory doesn't have the neigh territory
-        if(!current_viewed_territory_name.contains(neigh.getName())){
-          current_viewed_territory.add(neigh);
-          current_viewed_territory_name.add(neigh.getName());
-        }
-      }
-    }
-
-    // 3. Prepare to return 
-    HashMap<String, ArrayList<Territory>> current_new_map = new HashMap<String, ArrayList<Territory>>();
-    current_new_map.put("viewed", current_viewed_territory);
-    current_new_map.put("grey", current_grey_territory);
-    return current_new_map;
-  }
-
+  
     /**
      * @return a String that display the player color and their territories info
      */
@@ -190,4 +153,124 @@ public class Player implements java.io.Serializable{
       }
       return null;
   }
+    
+  /**
+   * viewMapInitializer() is called by the Player's constructor, and it can help the constructor to initialize old_view_map, new_view_map.
+   *@param player_terri_set is the latest information of the teritories owned by the player.
+   *@return HashMap<String, ArrayList<Territory>> curr_new_map. 
+   */
+  public HashMap<String, ArrayList<Territory>> viewMapInitializer(HashSet<Territory> player_terri_set){
+    ArrayList<Territory> cur_viewed_list = viewMapHelper(player_terri_set);
+    ArrayList<Territory> cur_grey_list = new ArrayList<Territory>();
+    // Prepare to return 
+    HashMap<String, ArrayList<Territory>> cur_new_map = new HashMap<String, ArrayList<Territory>>();
+    cur_new_map.put("viewed", cur_viewed_list);
+    cur_new_map.put("grey", cur_grey_list);
+    return cur_new_map;
+  }
+
+  private HashSet<String> getNameList(ArrayList<Territory> list){
+    HashSet<String> temp_name = new HashSet<String>();
+    for(Territory t : list){
+      temp_name.add(t.getName());
+    }
+    return temp_name;
+  }
+  
+  /**
+   * updateNewViewMap() is used when the new round starts. This method would be called by the Client, and then help the Client to update new_view_map by comparing the old_view_map which is from the last round.
+   *@return No return. This method will update HashMap<String, ArrayList<Territory>> new_view_map with cur_view_map.
+   */
+  public void updateNewViewMap(){
+    ArrayList<Territory> old_viewed_list = this.new_view_map.get("viewed");
+    ArrayList<Territory> old_grey_list = this.new_view_map.get("grey");
+    
+    ArrayList<Territory> cur_viewed_list = viewMapHelper(this.player_terri_set);
+    
+    ArrayList<Territory> cur_grey_list = new ArrayList<Territory>();
+    HashSet<String> cur_viewed_name = getNameList(cur_viewed_list);
+    HashSet<String> old_viewed_name = getNameList(old_viewed_list);
+    
+    // Logical part
+    for(Territory t : old_grey_list){
+      cur_grey_list.add(t.deep_copy());
+    }
+    //Start Comparing and Updating!
+    for(String s : cur_viewed_name){
+      if(!old_viewed_name.contains(s)){
+        // remove the territory(whose name is s) from cur_grey!
+        int index = 0;
+        // If there is no grey territory in the previous greylist.
+        if(cur_grey_list.size() == 0){
+          break;
+        }
+        for(;index < cur_grey_list.size(); index++){
+          Territory t = cur_grey_list.get(index);
+          if(t.getName().equals(s)){
+            break;
+          }
+        }
+        cur_grey_list.remove(index);
+      }
+    }
+    
+    for(String s : old_viewed_name){
+      System.out.println(s);
+      if(!cur_viewed_name.contains(s)){
+        // Move the territory(whose name is s) from old_viewed to cur_grey!
+        int index = 0;
+        for(; index < old_viewed_list.size(); index++){
+          Territory t = old_viewed_list.get(index);
+          if(t.getName().equals(s)){
+            break;
+          }
+        }
+        Territory t = old_viewed_list.get(index).deep_copy();
+        cur_grey_list.add(t);
+        
+      }
+    }
+    HashMap<String, ArrayList<Territory>> cur_new_map = new HashMap<String, ArrayList<Territory>>();    
+    cur_new_map.put("viewed", cur_viewed_list);
+    cur_new_map.put("grey", cur_grey_list);
+    this.new_view_map = cur_new_map;
+    }
+
+  /**
+   *viewMapHelper() is called by viewMapInitializer() and updateViewMap(). This method would deep copy the information from the last territories, and then traverse whole the neighbors from those territories. Finally, Add those neighbors into the return ArrayList.
+   *@param  player_terri_set is the latest information of the teritories owned by the player. 
+   *@return ArrayList<Territory> cur_viewed_list back to viewMapInitializer() or updateViewMap().
+   */
+  private ArrayList<Territory> viewMapHelper(HashSet<Territory> player_terri_set){
+    ArrayList<Territory> cur_viewed_list = new ArrayList<Territory>();
+    HashSet<String> cur_viewed_name = new HashSet<String>();
+
+    // 1. Adding the Territories that the player owns
+    for(Territory t : player_terri_set){
+      cur_viewed_list.add(t);
+      cur_viewed_name.add(t.getName());
+    }
+
+    // Adding the Neighbors of those Territories
+    // 2. Deep copy
+    ArrayList<Territory> current_viewed_territory_copy = new ArrayList<Territory>();
+    for(Territory t : cur_viewed_list){
+      Territory copy = t.deep_copy();
+      current_viewed_territory_copy.add(copy);
+    }
+    
+    // 3. Traverse Neighbors now
+    for(Territory terri : current_viewed_territory_copy){
+      for(Territory neigh : terri.getNeigh()){
+        // current_viewed_territory doesn't have the neigh territory
+        if(!cur_viewed_name.contains(neigh.getName())){
+          cur_viewed_list.add(neigh);
+          cur_viewed_name.add(neigh.getName());
+        }
+      }
+    }
+
+    return cur_viewed_list;
+  }
+
 }
